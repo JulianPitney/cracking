ab01597ee5f209ce9edccd49a005d39d4d9b70c2
Johnathan Martell <Martell-J@users.noreply.github.com>
2022-05-03 01:21:11 -0400

    Implemented singly-linked list

---
+++ john/data_structures/linked_list.py
@@ -0,0 +1,36 @@
+
+class Node:
+  def __init__(self, value):
+    self.value = value
+    self.next = None
+
+  def appendNext(self, next):
+    self.next = next
+    return next
+  
+class LinkedList:
+  def __init__(self, head, last):
+    self.head = head
+    self.last = last
+
+  def print(self):
+    
+    current = head
+    while (current != None):
+      print(current.value)
+      current = current.next
+
+head = None
+last = None
+
+for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+  if (head == None):
+    head = Node(i)
+    last = head
+  else:
+    last = last.appendNext(Node(i))
+
+
+list = LinkedList(head, last)
+
+list.print()
