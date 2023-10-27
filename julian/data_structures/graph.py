883ccd3e0c288cec82ff8a3ad50c8bc59a1a0d59
julian <julianpitney@gmail.com>
2023-10-27 18:51:35 -0400

    Completed 4.1 with depth first search

--- julian/data_structures/graph.py
+++ julian/data_structures/graph.py
@@ -26,36 +26,67 @@ def gen_directed_graph(size=10):
     for i in range(0, size):
 
         num_connections = randint(1, 3)
-        x = 0
-        while x < num_connections:
+        for _ in range(0, num_connections):
 
             connection = (i, randint(0, size - 1))
-            if connection == (i, i):
-                continue
-            elif connection in connections:
+            if connection == (i, i) or connection in connections:
                 continue
             else:
                 connections.append(connection)
-                x += 1
 
     # form connections using generated connections list
     for connection in connections:
+
         source = nodes[connection[0]]
         target = nodes[connection[1]]
-
         source.adjacent_nodes.append(target)
 
     return nodes
 
+def find_index_from_id(graph, nodeID):
 
-nodes = gen_directed_graph()
+    for i, node in enumerate(graph):
+
+        if nodeID == id(node):
+            return i
+
+    return None
 
 
-for i in range(0, 1000):
-    nodes = gen_directed_graph()
+def print_adjacency_list(graph):
 
-#for node in nodes:
-#    print(f"Node: {id(node)}")
-#    for anode in node.adjacent_nodes:
-#        print(f"Adjacent Node: {id(anode)}")
-#    print("\n")
+    for i, node in enumerate(graph):
+        print(f"Node{i}:")
+        for adjacent_node in node.adjacent_nodes:
+            print(f"    -> {find_index_from_id(graph, id(adjacent_node))}")
+
+
+
+
+def dfs(root: Node, target: Node, visited_nodes: dict):
+
+    visited_nodes[root] = True
+
+    for node in root.adjacent_nodes:
+
+        if node == target:
+            return True
+
+        if node in visited_nodes:
+            continue
+        else:
+            visited_nodes[node] = True
+            if dfs(node, target, visited_nodes):
+                return True
+            
+    return False        
+
+
+
+graph = gen_directed_graph(size=10)
+print_adjacency_list(graph)
+
+while 1:
+    i1 = int(input("Enter root index: "))
+    i2 = int(input("Enter target index: "))
+    print(f"Route Found: {dfs(graph[i1], graph[i2], {})}")
